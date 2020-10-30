6e69091f47e81605dee596f6e01246571eba2a68
Aleksa Trampa <alektramp@gmail.com>
2020-10-30 21:10:02 +0100

    added skeleton for signal device

---
+++ Assets/Game Jam/Signals/SignalDeviceInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalDeviceInfo : MonoBehaviour
+{
+    public List<SignalDevice> signalReceivers;
+    public float range;
+}
+
+public class SignalDeviceBroadcaster : MonoBehaviour
+{
+    public virtual void BroadcastSignal(SignalDevice receiver)
+    {
+        
+    }
+}
+
+public class SignalReceiver : MonoBehaviour
+{
+    public virtual void ReceiveSignal(SignalDevice broadcaster)
+    {
+        
+    }
+}
+
+public class SignalDevice : MonoBehaviour
+{
+    private SignalDeviceInfo info;
+    private SignalDeviceBroadcaster broadcaster;
+    private SignalReceiver receiver;
+
+    private void Awake()
+    {
+        info = GetComponent<SignalDeviceInfo>();
+        broadcaster = GetComponent<SignalDeviceBroadcaster>();
+        receiver = GetComponent<SignalReceiver>();
+    }
+}

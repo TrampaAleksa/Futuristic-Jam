ebd7e7d56500a5b8dbc7f414611a3f1f09c27a9f
Aleksa Trampa <alektramp@gmail.com>
2020-10-30 23:01:10 +0100

    broadcaster and receiver events

--- Assets/Game Jam/Signals/SignalDeviceBroadcaster.cs
+++ Assets/Game Jam/Signals/SignalDeviceBroadcaster.cs
@@ -1,24 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class SignalDeviceBroadcaster : MonoBehaviour
 {
+
+    private RaycastHit hit;
+    public UnityEvent<SignalDevice, SignalDevice> hitEvents;
+    public UnityEvent<SignalDevice, SignalDevice> wallEvents;
+
     public virtual void SendBroadcast(SignalDevice broadcaster)
     {
         foreach (var broadcastReceiver in broadcaster.info.broadcastReceivers)
         {
-            var receiverPosition = broadcastReceiver.transform.position;
-            var broadcasterPosition = broadcaster.transform.position;
-            
-            if (Physics.Linecast(broadcasterPosition, receiverPosition, LayerMask.GetMask("Wall")))
-            {
-                Debug.Log("blocked");
-            }
-            
-            broadcaster.info.line.SetPosition(0, broadcasterPosition);
-            broadcaster.info.line.SetPosition(1, receiverPosition);
-            
-            broadcastReceiver.receiver.ReceiveSignal(broadcaster);
+            if (SignalInterrupted(broadcaster, broadcastReceiver))
+                wallEvents?.Invoke(broadcaster, broadcastReceiver);
+            else
+                hitEvents?.Invoke(broadcaster, broadcastReceiver);
         }
         
     }
+    
+    private bool SignalInterrupted(SignalDevice broadcaster, SignalDevice receiver)
+    {
+        var receiverPosition = receiver.transform.position;
+        var broadcasterPosition = broadcaster.transform.position;
+        
+        return Physics.Linecast(broadcasterPosition, receiverPosition, out hit, LayerMask.GetMask("Wall"));
+    }
+
+    public void WallLineDraw(SignalDevice broadcaster,SignalDevice receiver)
+    {
+        var broadcasterPosition = broadcaster.transform.position;
+        var hitPoint = broadcaster.broadcaster.hit.point;
+        
+        broadcaster.info.line.SetPosition(0, broadcasterPosition);
+        broadcaster.info.line.SetPosition(1, hitPoint);
+    }
+
+    public void ReceiverLineDraw(SignalDevice broadcaster, SignalDevice receiver)
+    {
+        var receiverPosition = receiver.transform.position;
+        var broadcasterPosition = broadcaster.transform.position;
+        
+        broadcaster.info.line.SetPosition(0, broadcasterPosition);
+        broadcaster.info.line.SetPosition(1, receiverPosition);
+    }
+    
 }
--- Assets/Game Jam/Signals/SignalReceiver.cs
+++ Assets/Game Jam/Signals/SignalReceiver.cs
@@ -2,7 +2,8 @@
 
 public class SignalReceiver : MonoBehaviour
 {
-    public virtual void ReceiveSignal(SignalDevice broadcaster)
+    public virtual void ReceiveSignal(SignalDevice broadcaster, SignalDevice receiver = null)
     {
+        print(gameObject.name + " received signal from " + broadcaster.name);
     }
 }

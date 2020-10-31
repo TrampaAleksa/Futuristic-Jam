1cc77737056b0bd1a16bf2267b66a2635adb6c0e
Aleksa Trampa <alektramp@gmail.com>
2020-10-31 17:02:43 +0100

    device state skeleton methods

--- Assets/Game Jam/Signals/DeviceBroadcasting.cs
+++ Assets/Game Jam/Signals/DeviceBroadcasting.cs
@@ -3,4 +3,45 @@
     public DeviceBroadcasting(SDevice device) : base(device)
     {
     }
+
+    public override void Entry()
+    {
+        base.Entry();
+        ConnectToDevices();
+    }
+
+
+    private void ConnectToDevices()
+    {
+    }
+
+    public override void UpdateAction()
+    {
+        base.UpdateAction();
+        CheckConnections();
+        UpdateSignal();
+    }
+
+    private void CheckConnections()
+    {
+    }
+
+    private void UpdateSignal()
+    {
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        DisconnectFromDevices();
+        SilentMode();
+    }
+
+    private void SilentMode()
+    {
+    }
+
+    private void DisconnectFromDevices()
+    {
+    }
 }
--- Assets/Game Jam/Signals/DeviceSilent.cs
+++ Assets/Game Jam/Signals/DeviceSilent.cs
@@ -3,4 +3,35 @@
     public DeviceSilent(SDevice device) : base(device)
     {
     }
+
+    public override void Entry()
+    {
+        base.Entry();
+        StopSignals();
+    }
+
+    private void StopSignals()
+    {
+    }
+
+    public override void UpdateAction()
+    {
+        base.UpdateAction();
+        CheckForConnections();
+    }
+
+    private void CheckForConnections()
+    {
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StartBroadcasting();
+    }
+
+    private void StartBroadcasting()
+    {
+        
+    }
 }

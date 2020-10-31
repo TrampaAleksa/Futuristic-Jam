8406a174774a467380d7bfbd30e04dbe11fef4d7
Aleksa Trampa <alektramp@gmail.com>
2020-10-31 23:44:20 +0100

    deice pickup logic

--- Assets/Scripts/DevicePickup.cs
+++ Assets/Scripts/DevicePickup.cs
@@ -1,32 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class DevicePickup : MonoBehaviour
 {
-    private bool isPickedUp;
-    [SerializeField]
-    float sizeOfPickedUpDevice;
+    private static bool isPickedUp;
+    [SerializeField] float sizeOfPickedUpDevice;
 
-    [SerializeField]
-    float vratiDeviceNaVisinu = 1; 
+    [SerializeField] float vratiDeviceNaVisinu = 1;
 
     private GameObject robot;
+
+    private GameObject pickedUpDevice;
+
+    public GameObject deviceHolder;
+
+    private static string pickedUpDeviceName = " ";
+
     // Start is called before the first frame update
     void Start()
     {
         isPickedUp = false;
-        robot = GameObject.FindGameObjectWithTag("Player");        
+        robot = GameObject.FindGameObjectWithTag("Player");
+        pickedUpDevice = GameObject.FindGameObjectWithTag("PickedUpDevice");
     }
+
     // Update is called once per frame
     void Update()
-    {       
-        if(isPickedUp == true)
+    {
+        if (Input.GetAxis("Fire2") > 0)
         {
-            if (Input.GetAxis("Fire2") > 0)
+            if (isPickedUp == true && pickedUpDeviceName == gameObject.name)
             {
-                this.gameObject.transform.parent = null;
                 this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
                 this.gameObject.transform.position = new Vector3(gameObject.transform.position.x,
                     vratiDeviceNaVisinu, transform.position.z);
@@ -35,31 +42,36 @@ public class DevicePickup : MonoBehaviour
                 newScale.y = 1;
                 newScale.z = 1;
                 transform.localScale = newScale;
+                this.gameObject.transform.parent = deviceHolder.transform;
                 isPickedUp = false;
-                this.gameObject.GetComponent<Collider>().enabled = true;                
+                this.gameObject.GetComponent<Collider>().enabled = true;
             }
-        }           
+        }
     }
-    private void OnCollisionStay(Collision collision)
+
+    private void OnTriggerStay(Collider collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
-            print("Click left mouse button to pick device up!");           
+            if (isPickedUp == true) return;
+            print("Click left mouse button to pick device up!");
             if (Input.GetAxis("Fire1") > 0)
             {
                 this.gameObject.transform.parent = robot.transform;
-                this.gameObject.transform.position = GameObject.FindGameObjectWithTag("PickedUpDevice").transform.position;
+                this.gameObject.transform.position = pickedUpDevice.transform.position;
                 isPickedUp = true;
-                
+
                 // pokupljen device iznad glave               
                 Vector3 newScale = gameObject.transform.localScale;
                 newScale.x = sizeOfPickedUpDevice;
                 newScale.y = sizeOfPickedUpDevice;
                 newScale.z = sizeOfPickedUpDevice;
-                transform.localScale = newScale;               
+                transform.localScale = newScale;
                 this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-                this.gameObject.GetComponent<Collider>().enabled = false;               
+                this.gameObject.GetComponent<Collider>().enabled = false;
+
+                pickedUpDeviceName = gameObject.name;
             }
         }
-    }      
+    }
 }

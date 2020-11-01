e2c01fd1cec81215e9858f7d83ad47e70319126b
urosspasenovic <57662604+urosspasenovic@users.noreply.github.com>
2020-11-01 08:18:53 +0100

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnObsticle;
+    Transform destination;
+    [SerializeField]
+    float speed = 2f;
+    private int numberOfActiveDevices = 0;
+    private Transform target;
+    private bool isPlaced = false;
+    private float timeElapsed;
+    [SerializeField]
+    float timeTillDestroy;
+    NavMeshAgent navMeshAGent;
+    // Start is called before the first frame update
+    void Start()
+    {       
+        destination = FindClosestEnemy().transform;
+        navMeshAGent = this.GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaced == false)
+        {
+            transform.LookAt(destination);
+            if (destination == null)
+            {
+                gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                return;
+            }
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed > timeTillDestroy)
+            {
+                Destroy(this.gameObject);
+                Instantiate<GameObject>(spawnObsticle, destination.transform.position, Quaternion.identity);
+                destination.gameObject.tag = "Finish";
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targerVector = destination.transform.position;
+            navMeshAGent.SetDestination(targerVector);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+       
+        if (collision.gameObject.tag == "Player")
+            Destroy(this.gameObject);
+            
+        if (collision.gameObject.tag == "DeviceInPlace")
+        {
+            isPlaced = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+public GameObject FindClosestEnemy()
+{
+        
+    GameObject[] placedDevices;
+    placedDevices = GameObject.FindGameObjectsWithTag("DeviceInPlace");
+    GameObject closest;
+    int brojac;
+    int i;
+    numberOfActiveDevices = placedDevices.Length;
+    brojac = Random.Range(0, numberOfActiveDevices);
+    for (i = 0; i < brojac; i++)
+    {
+    }
+    closest = placedDevices[i];
+    return closest;
+    }
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+
+	public float minSecondsBetweenSpawning = 3.0f;
+	public float maxSecondsBetweenSpawning = 6.0f;
+	
+	private float timeElapsed;
+	private float timeTillDeploy;
+
+	// Use this for initialization
+	void Start () {
+		timeTillDeploy = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timeElapsed += Time.deltaTime;
+			if (timeTillDeploy < timeElapsed)
+			{
+				MakeThingToSpawn();
+				timeTillDeploy = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+				timeElapsed = 0;
+			}		
+	}
+
+	void MakeThingToSpawn()
+	{
+		GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+	}
+}

ca51b065cb2b7f32905ced8865533e6f2cf49979
urosspasenovic <57662604+urosspasenovic@users.noreply.github.com>
2020-11-07 00:34:10 +0100

    Update SpawnEnemies.cs

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -1,49 +1,59 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class SpawnEnemies : MonoBehaviour {
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnPrefab;
+    [SerializeField]
+    float minSecondsBetweenSpawning = 3.0f;
+    [SerializeField]
+    float maxSecondsBetweenSpawning = 6.0f;
+    [SerializeField]
+    GameObject firstSpawn;
+    [SerializeField]
+    GameObject secondSpawn;
+    [SerializeField]
+    GameObject thirdSpawn;
+    [SerializeField]
+    int maxEnemies;
+    private int number;
 
-	public GameObject spawnPrefab;
+    private float timeElapsed;
+    private float timeTillDeploy;
 
-	public float minSecondsBetweenSpawning = 3.0f;
-	public float maxSecondsBetweenSpawning = 6.0f;
-	
-	[SerializeField]
-	GameObject firstSpawn;
-	[SerializeField]
-	GameObject secondSpawn;
-	[SerializeField]
-	GameObject thirdSpawn;
-	private int number;
-	
-	private float timeElapsed;
-	private float timeTillDeploy;
+    // Use this for initialization
+    void Start()
+    {
+        timeTillDeploy = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+    }
 
-	// Use this for initialization
-	void Start () {
-		timeTillDeploy = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (FindObjectsOfType<Enemy>().Length < maxEnemies)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeTillDeploy < timeElapsed)
+            {
+                if (GameObject.FindGameObjectsWithTag("DeviceInPlace").Length > 0)
+                {
+                    MakeThingToSpawn();
+                    timeTillDeploy = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+                    timeElapsed = 0;
+                }
+            }
+        }
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		timeElapsed += Time.deltaTime;
-			if (timeTillDeploy < timeElapsed)
-			{
-				MakeThingToSpawn();
-				timeTillDeploy = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-				timeElapsed = 0;
-			}		
-	}
-
-	void MakeThingToSpawn()
-	{
-		var number = Random.Range(0, 3);
-		if (number == 0)
-			Instantiate<GameObject>(spawnPrefab, firstSpawn.gameObject.transform.position, transform.rotation);			
-		if (number == 1)
-			Instantiate<GameObject>(spawnPrefab, secondSpawn.gameObject.transform.position, transform.rotation);
-		if (number == 2)
-			Instantiate<GameObject>(spawnPrefab, thirdSpawn.gameObject.transform.position, transform.rotation);
-	}
+    void MakeThingToSpawn()
+    {
+        number = Random.Range(0, 3);
+        if (number == 0)
+            Instantiate<GameObject>(spawnPrefab, firstSpawn.gameObject.transform.position, transform.rotation);
+        if (number == 1)
+            Instantiate<GameObject>(spawnPrefab, secondSpawn.gameObject.transform.position, transform.rotation);
+        if (number == 2)
+            Instantiate<GameObject>(spawnPrefab, thirdSpawn.gameObject.transform.position, transform.rotation);
+    }
 }

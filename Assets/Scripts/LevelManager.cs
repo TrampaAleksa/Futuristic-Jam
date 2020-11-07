7e4a546b7aade0726b68eb842daab361726ab4ba
Marko Paunovic <marko.paunovic1998@gmail.com>
2020-11-07 21:46:56 +0100

    Scripts
    Player,LevelManager & SpawnPickUps

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,34 +5,29 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     GameObject teleport;
-    public void EnebleTeleport(int index)
-    {
-        teleport = GameObject.FindGameObjectWithTag("teleport" + index);
-        teleport.gameObject.GetComponent<Teleport>().enabled = true;
-    }
-    public void DisableTeleport(int index)
-    {
-        teleport = GameObject.FindGameObjectWithTag("teleport" + index);
-        teleport.gameObject.GetComponent<Teleport>().enabled = false;
-    }
+    Teleport teleport0, teleport1;
+    //public void EnebleTeleport(int index)
+    //{
+    //    teleport = GameObject.FindGameObjectWithTag("teleport" + index);
+    //    teleport.gameObject.GetComponent<Teleport>().enabled = true;
+    //}
+    //public void DisableTeleport(int index)
+    //{
+    //    teleport = GameObject.FindGameObjectWithTag("teleport" + index);
+    //    teleport.gameObject.GetComponent<Teleport>().enabled = false;
+    //}
 
     private void Update()
     {
-        if (Player.teleports[0] == 0)
+     if(Player.teleport == 0)
         {
-            DisableTeleport(0);
+            teleport0.enabled = false;
+            teleport1.enabled = false;
         }
         else
         {
-            EnebleTeleport(0);
-        }
-        if(Player.teleports[1] == 0)
-        {
-            DisableTeleport(0);
-        }
-        else
-        {
-            EnebleTeleport(0);
+            teleport0.enabled = true;
+            teleport1.enabled = true;
         }
     }
 }

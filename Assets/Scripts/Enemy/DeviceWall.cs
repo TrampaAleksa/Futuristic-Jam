996e5c5f6e432a03dd4727a7c3da86247aab7451
Uros Spasenovic <urosspasenovic9@gmail.com>
2020-11-08 20:43:29 +0100

    enemy skripte

---
+++ Assets/Scripts/Enemy/DeviceWall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceWall : MonoBehaviour
+{
+    [SerializeField]
+    GameObject device;
+    Vector3 startingPosition;
+    [SerializeField]
+    float speedOfRasingWall = 0.025f;
+    bool startedRasingWall = false;
+    void Start()
+    {
+        startingPosition = device.transform.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (device.transform.localPosition.y < 0f)
+        {
+             if (other.gameObject.tag == "Enemy")
+            {
+                device.transform.Translate(Vector3.up * speedOfRasingWall);
+                startedRasingWall = true;
+            }
+            else if(startedRasingWall)
+            {
+                device.transform.position = startingPosition;
+                startedRasingWall = false;
+            }
+        }
+        else startedRasingWall = false;
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Enemy/DeviceWallDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceWallDown : MonoBehaviour
+{
+    [SerializeField]
+    GameObject device;
+    Vector3 startingPosition;
+    [SerializeField]
+    float speedOfRasingWall = 0.025f;
+    [SerializeField]
+    float timesSlowerMovingWall = 5;
+    bool startedRasingWall = false;
+
+
+
+    void Start()
+    {
+        startingPosition = device.transform.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(device.transform.localPosition.y >= -2.63 && other.gameObject.tag == "Player")
+        {
+            device.transform.Translate(Vector3.down * speedOfRasingWall / timesSlowerMovingWall);
+            if (device.transform.localPosition.y < -0.78)
+                gameObject.tag = "DeviceInPlace";
+        }      
+    }
+}

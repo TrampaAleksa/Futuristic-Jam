b6e30c5b28a3bd6c0f93db11f64b4dd48c39c21e
Aleksa Trampa <alektramp@gmail.com>
2020-11-21 00:05:49 +0100

    dialogue handler script

---
+++ Assets/Scripts/DialogueHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueHandler : MonoBehaviour
+{
+   public Text dialogue;
+   public GameObject dialogueHolder;
+
+   public static DialogueHandler Instance;
+   private TimedAction timedAction;
+
+
+   private void Awake()
+   {
+      Instance = this;
+      timedAction = gameObject.AddComponent<TimedAction>();
+   }
+
+   public void ShowDialogue(string text, float time)
+   {
+      dialogue.text = text;
+      dialogueHolder.SetActive(true);
+      timedAction.StartTimedAction(() => dialogueHolder.SetActive(false), time);
+   }
+
+   public void ShowDialogue(string text)
+   {
+      dialogue.text = text;
+      dialogueHolder.SetActive(true);
+   }
+   
+}

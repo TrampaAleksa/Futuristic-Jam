cc4e2618910cdfe3adc64018018aa2f0f79524ad
Aleksa Trampa <alektramp@gmail.com>
2020-11-21 22:08:02 +0100

    added sounds for power ups

---
+++ Assets/Game Jam/Final/Sounds/PowerUpSounds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSounds : MonoBehaviour
+{
+    public AudioSource speedUp;
+    public AudioSource speedDown;
+    public AudioSource timeDown;
+    public AudioSource timeUp;
+    public AudioSource teleportOn;
+    public AudioSource teleportOff;
+
+    public static PowerUpSounds Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
---
+++ Assets/Game Jam/Final/Sounds/PowerupSoundTriggers.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PowerupSoundTriggers : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject help = other.gameObject;
+
+        if (other.gameObject.CompareTag("ms+"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.speedUp.Play();
+        }
+        else if (other.gameObject.CompareTag("ms-"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.speedDown.Play();
+        }
+        else if (other.gameObject.CompareTag("timer+"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.timeUp.Play();
+        }
+        else if (other.gameObject.CompareTag("timer-"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.timeDown.Play();
+        }
+        else if (other.gameObject.CompareTag("teleport+"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.teleportOn.Play();
+        }
+        else if (other.gameObject.CompareTag("teleport-"))
+        {
+            Debug.Log("power up: " + other.tag);
+            PowerUpSounds.Instance.teleportOff.Play();
+        }
+    }
+}

e0054d0724cffeaaf0e51f8da1c14c1cb46cf1ab
Aleksa Trampa <alektramp@gmail.com>
2020-11-21 22:33:12 +0100

    powerup sound delay

--- Assets/Game Jam/Final/Sounds/PowerupSoundTriggers.cs
+++ Assets/Game Jam/Final/Sounds/PowerupSoundTriggers.cs
@@ -5,36 +5,37 @@ public class PowerupSoundTriggers : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         GameObject help = other.gameObject;
+        var delay = 0.4f;
 
         if (other.gameObject.CompareTag("ms+"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.speedUp.Play();
+            PowerUpSounds.Instance.speedUp.PlayDelayed(delay);
         }
         else if (other.gameObject.CompareTag("ms-"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.speedDown.Play();
+            PowerUpSounds.Instance.speedDown.PlayDelayed(delay);
         }
         else if (other.gameObject.CompareTag("timer+"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.timeUp.Play();
+            PowerUpSounds.Instance.timeUp.PlayDelayed(delay);
         }
         else if (other.gameObject.CompareTag("timer-"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.timeDown.Play();
+            PowerUpSounds.Instance.timeDown.PlayDelayed(delay);
         }
         else if (other.gameObject.CompareTag("teleport+"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.teleportOn.Play();
+            PowerUpSounds.Instance.teleportOn.PlayDelayed(delay);
         }
         else if (other.gameObject.CompareTag("teleport-"))
         {
             Debug.Log("power up: " + other.tag);
-            PowerUpSounds.Instance.teleportOff.Play();
+            PowerUpSounds.Instance.teleportOff.PlayDelayed(delay);
         }
     }
 }

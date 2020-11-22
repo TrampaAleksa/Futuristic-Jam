e8a001839a2edb943c4c6f555fa7faf73dbe919c
Uros Spasenovic <urosspasenovic9@gmail.com>
2020-11-22 22:44:27 +0100

    Final touch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,13 +24,11 @@ public class Player : MonoBehaviour {
     private void FixedUpdate()
     {
         if (Input.GetAxis("Vertical") != 0)
-            rb.MovePosition(transform.position + transform.forward * (Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime));
-    }
-    void Update()
-    {
+            rb.MovePosition(transform.position + transform.forward * (Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime));
+       
         if (Input.GetAxis("Horizontal") != 0)
         {
-            transform.Rotate(0, rotationSpeed * 20f * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+            transform.Rotate(0, rotationSpeed * 20f * Input.GetAxis("Horizontal") * Time.fixedDeltaTime, 0);
         }
     }
 
